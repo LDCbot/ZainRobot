5fa8f0a604e08da4814164ac4a25912c5aec5d40
ᴛʜᴇ ᴀɴᴏɴʏᴍᴏᴜs <TheAnonymous2510@gmail.com>
2022-11-26 21:14:53 +0530

    Ah...

--- FallenRobot/modules/gitinfo.py
+++ FallenRobot/modules/gitinfo.py
@@ -1,28 +1,17 @@
-# © @AnonymousBoy1025
-import aiohttp
 from pyrogram import filters
 
-from FallenRobot import pbot
-from FallenRobot.pyrogramee.errors import capture_err
-
-__mod_name__ = "Gɪᴛʜᴜʙ"
-
-__help__ = """
-I will give information about github profile 
-
- ❍ /github <username>*:* Get information about a GitHub user.
-"""
+from FallenRobot import pbot, aiohttpsession
+from FallenRobot.utils.errors import capture_err
 
 
 @pbot.on_message(filters.command("github"))
 @capture_err
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git username")
-        return
+        return await message.reply_text("/git username")
     username = message.text.split(None, 1)[1]
     URL = f"https://api.github.com/users/{username}"
-    async with aiohttp.ClientSession() as session:
+    async with aiohttpsession as session:
         async with session.get(URL) as request:
             if request.status == 404:
                 return await message.reply_text("404")
@@ -41,16 +30,25 @@ async def github(_, message):
                 followers = result["followers"]
                 following = result["following"]
                 caption = f"""**Info Of {name}**
-**Username:** `{username}`
-**Bio:** `{bio}`
-**Profile Link:** [Here]({url})
-**Company:** `{company}`
-**Created On:** `{created_at}`
-**Repositories:** `{repositories}`
-**Blog:** `{blog}`
-**Location:** `{location}`
-**Followers:** `{followers}`
-**Following:** `{following}`"""
+**Username :** `{username}`
+**Bio :** `{bio}`
+**Profile Link :** [Here]({url})
+**Company :** `{company}`
+**Created On :** `{created_at}`
+**Repositories :** `{repositories}`
+**Blog :** `{blog}`
+**Location :** `{location}`
+**Followers :** `{followers}`
+**Following :** `{following}`"""
             except Exception as e:
                 print(str(e))
     await message.reply_photo(photo=avatar_url, caption=caption)
+
+
+__mod_name__ = "Gɪᴛʜᴜʙ"
+
+__help__ = """
+I will give information about github profile 
+
+ ❍ /github <username>*:* Get information about a GitHub user.
+"""
